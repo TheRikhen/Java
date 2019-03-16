0e991095f3f68ef61849c0ad84ef8c919bb11987
= <rikhen@icloud.com>
2019-03-16 23:01:28 +0300

    New project, using packages without import

---
+++ Java_5/src/Package_1/AAA.java
@@ -0,0 +1,12 @@
+package Package_1;
+
+public class AAA {
+    public int randvar;
+    public AAA(int x)
+    {
+        randvar = x;
+    }
+    public void Infout() {
+        System.out.println("Package1, AAA: " + randvar);
+    }
+}
---
+++ Java_5/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Package_1.AAA aaa = new Package_1.AAA(11);
+        aaa.Infout();
+        Package_1.Package_2.BBB bbb = new Package_1.Package_2.BBB(22);
+        bbb.Infout();
+        Package_1.Package_2.DDD ddd = new Package_1.Package_2.DDD(23);
+        ddd.Infout();
+        Package_1.Package_2.Package_3.Package_4.CCC ccc = new Package_1.Package_2.Package_3.Package_4.CCC(33);
+        ccc.Infout();
+        Package_1.Package_2.Package_3.Package_4.FFF fff = new Package_1.Package_2.Package_3.Package_4.FFF(34);
+        ccc.Infout();
+    }
+}
